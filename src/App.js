a3e05d2ebc6c7ab96cb5d220f61fc13720be52ac
Mohamed Aden <mo-aden@Mohameds-MBP.hsd1.co.comcast.net>
2022-03-28 22:37:45 -0600

    Bears updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello world! Brothers & Sisters 
+        Hello world! Brothers & Sisters - #TheBears
         </p>
         <a
           className="App-link"
